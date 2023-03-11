89f66764c3e08c93c7691f6fe927f65106a070a5
Suzan Alamassi <Suzanamassy@gmail.com>
2023-03-11 08:30:58 +0200

    Initial Commit

---
+++ ReflectiveUI6/ReflectiveUI6App.swift
@@ -0,0 +1,17 @@
+//
+//  ReflectiveUI6App.swift
+//  ReflectiveUI6
+//
+//  Created by Suzan Amassy on 11/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct ReflectiveUI6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
